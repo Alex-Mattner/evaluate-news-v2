d3172b4bab38f1a830c383cedfe4418537803422
Alex-Mattner <alex.mattner@gmx.de>
2021-09-27 12:32:35 +0200

    API post request working

--- src/server/index.js
+++ src/server/index.js
@@ -1,46 +1,48 @@
 var path = require('path')
 const express = require('express')
-const bodyParser = require('body-parser');
-//const mockAPIResponse = require('./mockAPI.js')
+var cors = require('cors')
+
+const axios = require('axios')
+const FormData = require('form-data');
+
 
 const app = express()
-
-app.use(express.static('dist'))
+app.use(cors())
+app.use(express.json())
 
 console.log(__dirname)
 
+ app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
+
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    // Code vorher: res.sendFile(path.resolve('src/client/views/index.html'))
-    res.sendFile(path.resolve('dist/index.html'))
+    res.send('api')
 })
+
+app.post('/getTranslation', async (req, res) => {
+    const txt = req.body.txt
+    const response = await getTranslationFromApi(txt)
+    console.log(response)
+    res.send(response)
+})
+
+const getTranslationFromApi = async (txt) => {
+    const formdata = new FormData();
+    formdata.append("key", "30c6dda9499377485d59ad4eec48ff34");
+    formdata.append("txt", txt);
+    const url = 'https://api.meaningcloud.com/lang-4.0/identification'
+    try {
+        const res = await axios.post(url, formdata, { headers: formdata.getHeaders()} )
+        return res.data
+    } catch(err) {
+        return err
+    }
+}
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
-
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
-  
-
-
-
-const formdata = new FormData();
-formdata.append("key", "30c6dda9499377485d59ad4eec48ff34");
-formdata.append("txt", "YOUR TEXT HERE");
-
-const requestOptions = {
-  method: 'POST',
-  body: formdata,
-  redirect: 'follow'
-};
-
-const response = fetch("https://api.meaningcloud.com/lang-4.0/identification", requestOptions)
-  .then(response => ({
-    status: response.status, 
-    body: response.json()
-  }))
-  .then({status, body} => console.log(status, body))
-  .catch(error => console.log('error', error));
